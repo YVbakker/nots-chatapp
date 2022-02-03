dde0d7c4f8865a7d1151da80389351455208d282
Yael Bakker <yv.bakker@student.han.nl>
2022-02-03 10:38:26 +0100

    Add boilerplate application

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatClient;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatClient;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void btnConnect_Click(object sender, EventArgs e) { }
+
+    private void btnSend_Click(object sender, EventArgs e) { }
+}
+
