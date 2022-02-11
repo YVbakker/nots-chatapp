0fabd7f54f49518f2409b14cd848ac01821bdb57
Yael Bakker <yv.bakker@student.han.nl>
2022-02-11 09:25:15 +0100

    Modify UI, play around with maui xaml

--- ChatClient/MainPage.xaml.cs
+++ ChatClient/MainPage.xaml.cs
@@ -1,14 +1,37 @@
-﻿namespace ChatClient;
+﻿using System.Collections.ObjectModel;
+
+namespace ChatClient;
 
 public partial class MainPage : ContentPage
 {
+    public List<string> chatbox = new List<string>();
     public MainPage()
     {
         InitializeComponent();
+        listChats.Header = "Messages";
     }
 
-    private void btnConnect_Click(object sender, EventArgs e) { }
+    private IEnumerable<string> Chat(string message)
+    {
+        chatbox.Add(message);
+        return chatbox;
+    }
 
-    private void btnSend_Click(object sender, EventArgs e) { }
+    private async void btnConnect_Click(object sender, EventArgs e)
+    {
+        MauiProgram.Client = new Client(int.Parse(txtPort.Text), txtIPServer.Text);
+        await MauiProgram.Client.Start();
+        if (MauiProgram.Client.Connected())
+        {
+            btnConnect.IsEnabled = false;
+            btnConnect.BackgroundColor = Color.FromRgb(255, 154, 154);
+            btnConnect.Text = "Disconnect";
+            listChats.ItemsSource = Chat("Client is connected!");
+        }
+    }
+
+    private void btnSend_Click(object sender, EventArgs e)
+    {
+        listChats.ItemsSource = Chat("Message sent!");
+    }
 }
-
--- ChatClient/MauiProgram.cs
+++ ChatClient/MauiProgram.cs
@@ -1,9 +1,19 @@
-﻿namespace ChatClient;
+﻿using System.Collections.ObjectModel;
+using Serilog;
+
+namespace ChatClient;
 
 public static class MauiProgram
 {
+    public static Client Client;
+    public static ObservableCollection<String> Messages { get; set; }
     public static MauiApp CreateMauiApp()
     {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+        
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
