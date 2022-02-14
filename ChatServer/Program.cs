e05e27bc0cc59f759bf98b0989ea046318d657ce
Yael Bakker <yv.bakker@student.han.nl>
2022-02-14 17:51:25 +0100

    Add Logger configuration

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Serilog;
+
 Console.WriteLine("Hello, World!");
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger = log;
+Log.Information("The global logger has been configured");
