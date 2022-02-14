040e3b95c79b5f2a79232e80273b7c9698985f24
Yael Bakker <yv.bakker@student.han.nl>
2022-02-14 18:28:49 +0100

    Add Logging, Dns resolution

--- ChatServer/Server.cs
+++ ChatServer/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using Serilog;
 
 namespace ChatServer;
 
@@ -7,13 +8,16 @@ public class Server
 {
     private const int Backlog = 100;
     private int _bufferSize;
+    private readonly IPHostEntry _hostEntry;
     private readonly IPEndPoint _endPoint;
     private readonly Socket _listenSocket;
     private IList<Socket> _clients;
 
-    public Server(IPAddress address, int port, int bufferSize)
+    public Server(string hostname, int port, int bufferSize)
     {
         _bufferSize = bufferSize;
+        _hostEntry = Dns.GetHostEntry(hostname);
+        var address = _hostEntry.AddressList.FirstOrDefault() ?? IPAddress.Loopback;
         _endPoint = new IPEndPoint(address, port);
         _listenSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         _clients = new List<Socket>();
@@ -21,8 +25,16 @@ public class Server
 
     public void Start()
     {
-        StartListeningAsync().Start();
-        
+        try
+        {
+            StartListeningAsync().Start();
+            Log.Information("Server started");
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Could not start server");
+            throw;
+        }
     }
 
     private async Task StartListeningAsync()
@@ -33,13 +45,15 @@ public class Server
             _listenSocket.Listen(Backlog);
             while (true)
             {
+                Log.Information("Listening for new connections on {Hostname}:{Port}", _hostEntry.HostName, _endPoint.Port);
                 var newClient = await _listenSocket.AcceptAsync();
                 _clients.Add(newClient);
+                Log.Information("New client added ({@RemoteAddress})", newClient.RemoteEndPoint);
             }
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
+            Log.Error(e, "Could not start listener thread");
             throw;
         }
     }
