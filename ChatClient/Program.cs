3ac36a91c531f6c827b81b9a520dcd5b818408ef
Yael Bakker <yv.bakker@student.han.nl>
2022-02-21 17:28:21 +0100

    Add cli arguments to console client/server

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -2,16 +2,99 @@
 
 using ChatClient;
 using Serilog;
+using System;
 
-using var log = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .CreateLogger();
 
-Log.Logger = log;
-Log.Information("The global logger has been configured");
-Log.Information("Attempting to connect client to server");
-var client = new Client("localhost", 9000, 1024);
+namespace ChatClient // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            using var log = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-await client.Connect();
-await Task.WhenAll(client.StartSending(), client.StartReceiving());
+            Log.Logger = log;
+
+            var hostname = "localhost";
+            var port = 9000;
+            var bufferSize = 1024;
+
+            switch (args.Length)
+            {
+                case 0:
+                    Console.WriteLine("Usage: chatClient <hostname> <port> <buffersize>");
+                    return 1;
+                case 1:
+                    try
+                    {
+                        hostname = args[0];
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e, "Please enter a valid string for hostname");
+                        throw;
+                    }
+
+                    break;
+                case 2:
+                    try
+                    {
+                        hostname = args[0];
+                        port = int.Parse(args[1]);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e, "Please enter a valid string for hostname, number for port");
+                        throw;
+                    }
+
+                    break;
+                case 3:
+                    try
+                    {
+                        hostname = args[0];
+                        port = int.Parse(args[1]);
+                        bufferSize = int.Parse(args[2]);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e,
+                            "Please enter a valid string for hostname and a number for portnumber, buffersize");
+                        throw;
+                    }
+
+                    hostname = args[0];
+                    break;
+            }
+
+            Log.Information("The global logger has been configured");
+            Log.Information("Attempting to connect client to server");
+            Client client;
+            try
+            {
+                client = new Client(hostname, port, bufferSize);
+                await client.Connect();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Could not connect to server");
+                throw;
+            }
+
+            try
+            {
+                await Task.WhenAll(client.StartSending(), client.StartReceiving());
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Could not start sender or receiver thread");
+                throw;
+            }
+
+            return 0;
+        }
+    }
+}
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -3,15 +3,86 @@
 using ChatServer;
 using Serilog;
 
-using var log = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .Enrich.WithThreadId()
-    .WriteTo.Console()
-    .CreateLogger();
+namespace ChatServer // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            using var log = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-Log.Logger = log;
-Log.Information("The global logger has been configured");
-Log.Information("Attempting to start the server...");
-var server = new Server("localhost", 9000, 1024);
-await server.Start();
+            Log.Logger = log;
+
+            var hostname = "localhost";
+            var port = 9000;
+            var bufferSize = 1024;
+
+            switch (args.Length)
+            {
+                case 0:
+                    Console.WriteLine("Usage: chatServer <hostname> <port> <buffersize>");
+                    return 1;
+                case 1:
+                    try
+                    {
+                        hostname = args[0];
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e, "Please enter a valid string for hostname");
+                        throw;
+                    }
+
+                    break;
+                case 2:
+                    try
+                    {
+                        hostname = args[0];
+                        port = int.Parse(args[1]);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e, "Please enter a valid string for hostname, number for port");
+                        throw;
+                    }
+
+                    break;
+                case 3:
+                    try
+                    {
+                        hostname = args[0];
+                        port = int.Parse(args[1]);
+                        bufferSize = int.Parse(args[2]);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e,
+                            "Please enter a valid string for hostname and a number for portnumber, buffersize");
+                        throw;
+                    }
+
+                    hostname = args[0];
+                    break;
+            }
+
+            Log.Information("The global logger has been configured");
+            Log.Information("Attempting to start the server...");
+            Server server;
+            try
+            {
+                server = new Server(hostname, port, bufferSize);
+                await server.Start();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Could not start server");
+                throw;
+            }
+            return 0;
+        }
+    }
+}
 
